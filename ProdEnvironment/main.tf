5e4bd6605ade1fa7525ac6660a8a9b36736be70c
Nevralgie <93102912+Nevralgie@users.noreply.github.com>
2023-06-15 16:48:49 +0200

    Update main.tf

--- ProdEnvironment/main.tf
+++ ProdEnvironment/main.tf
@@ -13,7 +13,11 @@ provider "azurerm" {
    client_secret = var.client_secret
    tenant_id = var.tenant_id
    skip_provider_registration = true
-   features {}
+   features {
+      resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 
